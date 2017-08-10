5974ae9f638435070aee4f51eb85971578bb42fb
Sergii Smyrnov <smirnoffs@gmail.com>
2017-08-10 21:56:18 +0200

    make possible to create multiple gift cards

--- karmin/giftcards/views.py
+++ karmin/giftcards/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from karmin.giftcards.models import GiftCard
@@ -7,3 +9,14 @@ from karmin.giftcards.serializers import GiftCardSerializer
 class GiftCardViewSet(ModelViewSet):
     queryset = GiftCard.objects.all()
     serializer_class = GiftCardSerializer
+
+    def create(self, request, *args, **kwargs):
+        many = False
+        request_data = request.data
+        if isinstance(request.data, list):
+            many = True
+        serializer = self.get_serializer(data=request_data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
